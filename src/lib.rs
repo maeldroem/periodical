b042383a5592d2c6e838bbe3765cd4a2d0bf63fb
maeldroem <maeldroem@proton.me>
2025-08-10 23:54:52 +0200

    Created crate prelude

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 pub mod collections;
 pub mod intervals;
 pub mod ops;
+pub mod prelude;
 pub mod scheduling;
 
 #[cfg(feature = "serde")]
---
+++ src/prelude.rs
@@ -0,0 +1,29 @@
+pub use crate::intervals::absolute::{
+    AbsoluteBounds, AbsoluteInterval, ClosedAbsoluteInterval, EmptiableAbsoluteBounds, HalfOpenAbsoluteInterval,
+    HasAbsoluteBounds, HasEmptiableAbsoluteBounds,
+};
+pub use crate::intervals::meta::{Emptiable, HasBoundInclusivity, HasDuration, HasOpenness, HasRelativity};
+pub use crate::intervals::ops::abridge::Abridgable;
+pub use crate::intervals::ops::complement::Complementable;
+pub use crate::intervals::ops::cut::{CutResult, CutType, Cuttable};
+pub use crate::intervals::ops::extend::Extensible;
+pub use crate::intervals::ops::fill_gap::GapFillable;
+pub use crate::intervals::ops::grow::{GrowableEndBound, GrowableStartBound};
+pub use crate::intervals::ops::overlap::{
+    CanPositionOverlap, DEFAULT_OVERLAP_RULES, DisambiguatedOverlapPosition, OverlapPosition, OverlapRule,
+    OverlapRuleSet,
+};
+pub use crate::intervals::ops::precision::PreciseAbsoluteBounds;
+pub use crate::intervals::ops::relativity_conversion::{ToAbsolute, ToRelative};
+pub use crate::intervals::ops::remove_overlap::{OverlapRemovable, OverlapRemovalResult};
+pub use crate::intervals::ops::remove_overlap_or_gap::{OverlapOrGapRemovalResult, RemovableOverlapOrGap};
+pub use crate::intervals::ops::set_ops::{Differentiable, Intersectable, SymmetricallyDifferentiable, Unitable};
+pub use crate::intervals::ops::shrink::{ShrinkableEndBound, ShrinkableStartBound};
+pub use crate::intervals::ops::time_containment::{
+    CanPositionTimeContainment, DEFAULT_TIME_CONTAINMENT_RULES, DisambiguatedTimeContainmentPosition,
+    TimeContainmentPosition, TimeContainmentRule, TimeContainmentRuleSet,
+};
+pub use crate::intervals::relative::{
+    ClosedRelativeInterval, EmptiableRelativeBounds, HalfOpenRelativeInterval, HasEmptiableRelativeBounds,
+    HasRelativeBounds, RelativeInterval,
+};
