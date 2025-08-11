f8e66a2f49745b9cb79fa104ca9b152c963711fe
maeldroem <maeldroem@proton.me>
2025-08-11 22:32:13 +0200

    Added fuzz test for abridging 2nd degree complement of a closed interval

---
+++ fuzz/fuzz_targets/abridge_2nd_degree_complement_of_closed_interval.rs
@@ -0,0 +1,25 @@
+#![no_main]
+
+use libfuzzer_sys::fuzz_target;
+use periodical::prelude::*;
+
+fuzz_target!(|closed_interval: ClosedAbsoluteInterval| {
+    let (left_complement, right_complement) = closed_interval
+        .complement()
+        .split()
+        .expect("The complement of a closed interval always results in a split complement");
+
+    let left_2nd_degree_complement = left_complement
+        .complement()
+        .single()
+        .expect("The complement of a half-open interval always results in a single complement");
+
+    let right_2nd_degree_complement = right_complement
+        .complement()
+        .single()
+        .expect("The complement of a half-open interval always results in a single complement");
+
+    let abridgment_2nd_degree_complements = left_2nd_degree_complement.abridge(&right_2nd_degree_complement);
+
+    assert_eq!(abridgment_2nd_degree_complements, AbsoluteInterval::from(closed_interval));
+});
