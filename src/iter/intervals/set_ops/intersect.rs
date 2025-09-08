d9c1ac10507fb85ec9bab0567acbb1c4bfe54b88
maeldroem <maeldroem@proton.me>
2025-09-08 03:19:24 +0200

    Added size hint and rippled iter trait bounds for intersection iter

--- src/iter/intervals/set_ops/intersect.rs
+++ src/iter/intervals/set_ops/intersect.rs
@@ -5,37 +5,6 @@ use std::iter::{FusedIterator, Peekable};
 use crate::intervals::prelude::*;
 use crate::ops::IntersectionResult;
 
-/// Dispatcher trait for peer intersection iterators
-pub trait PeerIntersectableIteratorDispatcher: IntoIterator + Sized {
-    /// Intersects peer intervals of the iterator using the default overlap rules
-    ///
-    /// Processes elements pair by pair and returns the result of the intersection. If the intersection is successful,
-    /// it returns the intersected interval. If it is unsuccessful, it returns the current element.
-    fn peer_intersection(self) -> PeerIntersection<Peekable<Self::IntoIter>> {
-        PeerIntersection::new(self.into_iter())
-    }
-
-    /// Intersects peer intervals of the iterator using the given closure
-    ///
-    /// Processes elements pair by pair and returns the result of the intersection. If the intersection is successful,
-    /// it returns the intersected interval. If it is unsuccessful, it returns the current element.
-    fn peer_intersection_with<'a, T, F>(self, f: F) -> PeerIntersectionWith<Peekable<Self::IntoIter>, F>
-    where
-        Self::IntoIter: Iterator<Item = &'a T>,
-        T: 'a + Intersectable<Output = T> + Clone,
-        F: FnMut(&T, &T) -> IntersectionResult<T>,
-    {
-        PeerIntersectionWith::new(self.into_iter(), f)
-    }
-}
-
-impl<'a, I, T> PeerIntersectableIteratorDispatcher for I
-where
-    I: IntoIterator<Item = &'a T>,
-    T: 'a + Intersectable<Output = T> + Clone,
-{
-}
-
 /// Peer intersection iterator for intervals using predefined rules
 #[derive(Debug, Clone, Hash)]
 pub struct PeerIntersection<I> {
@@ -43,9 +12,10 @@ pub struct PeerIntersection<I> {
     exhausted: bool,
 }
 
-impl<I> PeerIntersection<I>
+impl<'a, I, T, U> PeerIntersection<I>
 where
-    I: Iterator,
+    I: Iterator<Item = &'a T>,
+    T: 'a + Intersectable<Output = U> + Into<U> + Clone,
 {
     pub fn new(iter: I) -> PeerIntersection<Peekable<I>> {
         PeerIntersection {
@@ -55,12 +25,12 @@ where
     }
 }
 
-impl<'a, I, T> Iterator for PeerIntersection<Peekable<I>>
+impl<'a, I, T, U> Iterator for PeerIntersection<Peekable<I>>
 where
     I: Iterator<Item = &'a T>,
-    T: 'a + Intersectable<Output = T> + Clone,
+    T: 'a + Intersectable<Output = U> + Into<U> + Clone,
 {
-    type Item = T;
+    type Item = U;
 
     fn next(&mut self) -> Option<Self::Item> {
         if self.exhausted {
@@ -74,23 +44,51 @@ where
 
         let Some(peeked) = self.iter.peek() else {
             self.exhausted = true;
-            return Some(current.clone());
+            return Some(current.clone().into());
         };
 
         match current.intersect(peeked) {
             IntersectionResult::Intersected(intersected) => Some(intersected),
-            IntersectionResult::Separate => Some(current.clone()),
+            IntersectionResult::Separate => Some(current.clone().into()),
         }
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        self.iter.size_hint()
     }
 }
 
 // TODO: If a reverse Peekable becomes standard or when we'll import a crate that does that,
 // implement DoubleEndedIterator for PeerIntersection
 
-impl<'a, I, T> FusedIterator for PeerIntersection<Peekable<I>>
+impl<'a, I, T, U> FusedIterator for PeerIntersection<Peekable<I>>
 where
     I: Iterator<Item = &'a T>,
-    T: 'a + Intersectable<Output = T> + Clone,
+    T: 'a + Intersectable<Output = U> + Into<U> + Clone,
+{
+}
+
+/// Iterator dispatcher for [`PeerIntersection`]
+pub trait PeerIntersectionIteratorDispatcher<'a, T, U>
+where
+    Self: IntoIterator + Sized,
+    Self::IntoIter: Iterator<Item = &'a T>,
+    T: 'a + Intersectable<Output = U> + Into<U> + Clone,
+{
+    /// Intersects peer intervals of the iterator using the default overlap rules
+    ///
+    /// Processes elements pair by pair and returns the result of the intersection. If the intersection is successful,
+    /// it returns the intersected interval. If it is unsuccessful, it returns the current element.
+    fn peer_intersection(self) -> PeerIntersection<Peekable<Self::IntoIter>> {
+        PeerIntersection::new(self.into_iter())
+    }
+}
+
+impl<'a, I, T, U> PeerIntersectionIteratorDispatcher<'a, T, U> for I
+where
+    I: IntoIterator + Sized,
+    I::IntoIter: Iterator<Item = &'a T>,
+    T: 'a + Intersectable<Output = U> + Into<U> + Clone,
 {
 }
 
@@ -102,9 +100,11 @@ pub struct PeerIntersectionWith<I, F> {
     exhausted: bool,
 }
 
-impl<I, F> PeerIntersectionWith<I, F>
+impl<'a, I, T, U, F> PeerIntersectionWith<I, F>
 where
-    I: Iterator,
+    I: Iterator<Item = &'a T>,
+    T: 'a + Into<U> + Clone,
+    F: FnMut(&T, &T) -> IntersectionResult<U>,
 {
     pub fn new(iter: I, f: F) -> PeerIntersectionWith<Peekable<I>, F> {
         PeerIntersectionWith {
@@ -115,13 +115,13 @@ where
     }
 }
 
-impl<'a, I, T, F> Iterator for PeerIntersectionWith<Peekable<I>, F>
+impl<'a, I, T, U, F> Iterator for PeerIntersectionWith<Peekable<I>, F>
 where
     I: Iterator<Item = &'a T>,
-    T: 'a + Intersectable<Output = T> + Clone,
-    F: FnMut(&T, &T) -> IntersectionResult<T>,
+    T: 'a + Into<U> + Clone,
+    F: FnMut(&T, &T) -> IntersectionResult<U>,
 {
-    type Item = T;
+    type Item = U;
 
     fn next(&mut self) -> Option<Self::Item> {
         if self.exhausted {
@@ -135,23 +135,53 @@ where
 
         let Some(peeked) = self.iter.peek() else {
             self.exhausted = true;
-            return Some(current.clone());
+            return Some(current.clone().into());
         };
 
         match (self.f)(current, peeked) {
             IntersectionResult::Intersected(intersected) => Some(intersected),
-            IntersectionResult::Separate => Some(current.clone()),
+            IntersectionResult::Separate => Some(current.clone().into()),
         }
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        self.iter.size_hint()
     }
 }
 
 // TODO: If a reverse Peekable becomes standard or when we'll import a crate that does that,
 // implement DoubleEndedIterator for PeerIntersectionWith
 
-impl<'a, I, T, F> FusedIterator for PeerIntersectionWith<Peekable<I>, F>
+impl<'a, I, T, U, F> FusedIterator for PeerIntersectionWith<Peekable<I>, F>
 where
     I: Iterator<Item = &'a T>,
-    T: 'a + Intersectable<Output = T> + Clone,
-    F: FnMut(&T, &T) -> IntersectionResult<T>,
+    T: 'a + Into<U> + Clone,
+    F: FnMut(&T, &T) -> IntersectionResult<U>,
+{
+}
+
+/// Iterator dispatcher trait for [`PeerIntersectionWith`]
+pub trait PeerIntersectionWithIteratorDispatcher<'a, T, U, F>
+where
+    Self: IntoIterator + Sized,
+    Self::IntoIter: Iterator<Item = &'a T>,
+    T: 'a + Into<U> + Clone,
+    F: FnMut(&T, &T) -> IntersectionResult<U>,
+{
+    /// Intersects peer intervals of the iterator using the given closure
+    ///
+    /// Processes elements pair by pair and returns the result of the intersection. If the intersection is successful,
+    /// it returns the intersected interval. If it is unsuccessful, it returns the current element.
+    fn peer_intersection_with(self, f: F) -> PeerIntersectionWith<Peekable<Self::IntoIter>, F> {
+        PeerIntersectionWith::new(self.into_iter(), f)
+    }
+}
+
+impl<'a, I, T, U, F> PeerIntersectionWithIteratorDispatcher<'a, T, U, F> for I
+where
+    I: IntoIterator + Sized,
+    I::IntoIter: Iterator<Item = &'a T>,
+    T: 'a + Into<U> + Clone,
+    F: FnMut(&T, &T) -> IntersectionResult<U>,
 {
 }
