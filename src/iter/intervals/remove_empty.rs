c619966b9246821c8a3215b888b51916eec2bb2c
maeldroem <maeldroem@proton.me>
2025-09-19 21:17:57 +0200

    Added docs for empty interval removal iterator

--- src/iter/intervals/remove_empty.rs
+++ src/iter/intervals/remove_empty.rs
@@ -1,14 +1,164 @@
 //! Iterator to remove empty intervals
+//!
+//! This iterator uses the [`Emptiable`] trait to determine what is considered an empty interval.
+//!
+//! # Examples
+//!
+//! ```
+//! # use chrono::{DateTime, Utc};
+//! # use periodical::intervals::absolute::{
+//! #     AbsoluteBounds, AbsoluteEndBound, AbsoluteFiniteBound, AbsoluteStartBound, EmptiableAbsoluteBounds,
+//! # };
+//! # use periodical::iter::intervals::remove_empty::RemoveEmptyIntervalsIteratorDispatcher;
+//! let intervals = [
+//!     EmptiableAbsoluteBounds::Empty,
+//!     EmptiableAbsoluteBounds::Empty,
+//!     EmptiableAbsoluteBounds::Bound(AbsoluteBounds::new(
+//!         AbsoluteStartBound::Finite(AbsoluteFiniteBound::new(
+//!             "2025-01-01 08:00:00Z".parse::<DateTime<Utc>>()?,
+//!         )),
+//!         AbsoluteEndBound::Finite(AbsoluteFiniteBound::new(
+//!             "2025-01-01 12:00:00Z".parse::<DateTime<Utc>>()?,
+//!         )),
+//!     )),
+//!     EmptiableAbsoluteBounds::Empty,
+//!     EmptiableAbsoluteBounds::Bound(AbsoluteBounds::new(
+//!         AbsoluteStartBound::Finite(AbsoluteFiniteBound::new(
+//!             "2025-01-01 13:00:00Z".parse::<DateTime<Utc>>()?,
+//!         )),
+//!         AbsoluteEndBound::Finite(AbsoluteFiniteBound::new(
+//!             "2025-01-01 14:00:00Z".parse::<DateTime<Utc>>()?,
+//!         )),
+//!     )),
+//!     EmptiableAbsoluteBounds::Bound(AbsoluteBounds::new(
+//!         AbsoluteStartBound::Finite(AbsoluteFiniteBound::new(
+//!             "2025-01-01 15:00:00Z".parse::<DateTime<Utc>>()?,
+//!         )),
+//!         AbsoluteEndBound::Finite(AbsoluteFiniteBound::new(
+//!             "2025-01-01 18:00:00Z".parse::<DateTime<Utc>>()?,
+//!         )),
+//!     )),
+//!     EmptiableAbsoluteBounds::Empty,
+//!     EmptiableAbsoluteBounds::Empty,
+//!     EmptiableAbsoluteBounds::Empty,
+//! ];
+//!
+//! assert_eq!(
+//!     intervals.remove_empty_intervals().collect::<Vec<_>>(),
+//!     vec![
+//!         EmptiableAbsoluteBounds::Bound(AbsoluteBounds::new(
+//!             AbsoluteStartBound::Finite(AbsoluteFiniteBound::new(
+//!                 "2025-01-01 08:00:00Z".parse::<DateTime<Utc>>()?,
+//!             )),
+//!             AbsoluteEndBound::Finite(AbsoluteFiniteBound::new(
+//!                 "2025-01-01 12:00:00Z".parse::<DateTime<Utc>>()?,
+//!             )),
+//!         )),
+//!         EmptiableAbsoluteBounds::Bound(AbsoluteBounds::new(
+//!             AbsoluteStartBound::Finite(AbsoluteFiniteBound::new(
+//!                 "2025-01-01 13:00:00Z".parse::<DateTime<Utc>>()?,
+//!             )),
+//!             AbsoluteEndBound::Finite(AbsoluteFiniteBound::new(
+//!                 "2025-01-01 14:00:00Z".parse::<DateTime<Utc>>()?,
+//!             )),
+//!         )),
+//!         EmptiableAbsoluteBounds::Bound(AbsoluteBounds::new(
+//!             AbsoluteStartBound::Finite(AbsoluteFiniteBound::new(
+//!                 "2025-01-01 15:00:00Z".parse::<DateTime<Utc>>()?,
+//!             )),
+//!             AbsoluteEndBound::Finite(AbsoluteFiniteBound::new(
+//!                 "2025-01-01 18:00:00Z".parse::<DateTime<Utc>>()?,
+//!             )),
+//!         )),
+//!     ],
+//! );
+//! # Ok::<(), chrono::format::ParseError>(())
+//! ```
 
 use crate::intervals::Emptiable;
 
-/// Dispatcher trait for empty interval removal
+/// Dispatcher trait for empty interval removal iterator
 pub trait RemoveEmptyIntervalsIteratorDispatcher
 where
     Self: IntoIterator + Sized,
     Self::Item: Emptiable,
 {
-    /// Remove empty intervals
+    /// Filters out empty intervals from the collection
+    ///
+    /// Uses the trait [`Emptiable`] under the hood.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use chrono::{DateTime, Utc};
+    /// # use periodical::intervals::absolute::{
+    /// #     AbsoluteBounds, AbsoluteEndBound, AbsoluteFiniteBound, AbsoluteStartBound, EmptiableAbsoluteBounds,
+    /// # };
+    /// # use periodical::iter::intervals::remove_empty::RemoveEmptyIntervalsIteratorDispatcher;
+    /// let intervals = [
+    ///     EmptiableAbsoluteBounds::Empty,
+    ///     EmptiableAbsoluteBounds::Empty,
+    ///     EmptiableAbsoluteBounds::Bound(AbsoluteBounds::new(
+    ///         AbsoluteStartBound::Finite(AbsoluteFiniteBound::new(
+    ///             "2025-01-01 08:00:00Z".parse::<DateTime<Utc>>()?,
+    ///         )),
+    ///         AbsoluteEndBound::Finite(AbsoluteFiniteBound::new(
+    ///             "2025-01-01 12:00:00Z".parse::<DateTime<Utc>>()?,
+    ///         )),
+    ///     )),
+    ///     EmptiableAbsoluteBounds::Empty,
+    ///     EmptiableAbsoluteBounds::Bound(AbsoluteBounds::new(
+    ///         AbsoluteStartBound::Finite(AbsoluteFiniteBound::new(
+    ///             "2025-01-01 13:00:00Z".parse::<DateTime<Utc>>()?,
+    ///         )),
+    ///         AbsoluteEndBound::Finite(AbsoluteFiniteBound::new(
+    ///             "2025-01-01 14:00:00Z".parse::<DateTime<Utc>>()?,
+    ///         )),
+    ///     )),
+    ///     EmptiableAbsoluteBounds::Bound(AbsoluteBounds::new(
+    ///         AbsoluteStartBound::Finite(AbsoluteFiniteBound::new(
+    ///             "2025-01-01 15:00:00Z".parse::<DateTime<Utc>>()?,
+    ///         )),
+    ///         AbsoluteEndBound::Finite(AbsoluteFiniteBound::new(
+    ///             "2025-01-01 18:00:00Z".parse::<DateTime<Utc>>()?,
+    ///         )),
+    ///     )),
+    ///     EmptiableAbsoluteBounds::Empty,
+    ///     EmptiableAbsoluteBounds::Empty,
+    ///     EmptiableAbsoluteBounds::Empty,
+    /// ];
+    ///
+    /// assert_eq!(
+    ///     intervals.remove_empty_intervals().collect::<Vec<_>>(),
+    ///     vec![
+    ///         EmptiableAbsoluteBounds::Bound(AbsoluteBounds::new(
+    ///             AbsoluteStartBound::Finite(AbsoluteFiniteBound::new(
+    ///                 "2025-01-01 08:00:00Z".parse::<DateTime<Utc>>()?,
+    ///             )),
+    ///             AbsoluteEndBound::Finite(AbsoluteFiniteBound::new(
+    ///                 "2025-01-01 12:00:00Z".parse::<DateTime<Utc>>()?,
+    ///             )),
+    ///         )),
+    ///         EmptiableAbsoluteBounds::Bound(AbsoluteBounds::new(
+    ///             AbsoluteStartBound::Finite(AbsoluteFiniteBound::new(
+    ///                 "2025-01-01 13:00:00Z".parse::<DateTime<Utc>>()?,
+    ///             )),
+    ///             AbsoluteEndBound::Finite(AbsoluteFiniteBound::new(
+    ///                 "2025-01-01 14:00:00Z".parse::<DateTime<Utc>>()?,
+    ///             )),
+    ///         )),
+    ///         EmptiableAbsoluteBounds::Bound(AbsoluteBounds::new(
+    ///             AbsoluteStartBound::Finite(AbsoluteFiniteBound::new(
+    ///                 "2025-01-01 15:00:00Z".parse::<DateTime<Utc>>()?,
+    ///             )),
+    ///             AbsoluteEndBound::Finite(AbsoluteFiniteBound::new(
+    ///                 "2025-01-01 18:00:00Z".parse::<DateTime<Utc>>()?,
+    ///             )),
+    ///         )),
+    ///     ],
+    /// );
+    /// # Ok::<(), chrono::format::ParseError>(())
+    /// ```
     fn remove_empty_intervals(self) -> RemoveEmptyIntervals<Self::IntoIter> {
         RemoveEmptyIntervals::new(self.into_iter())
     }
@@ -22,6 +172,8 @@ where
 }
 
 /// Empty interval removal iterator
+///
+/// Uses the trait [`Emptiable`] in order to determine what is an empty interval.
 #[derive(Debug, Clone, Hash)]
 pub struct RemoveEmptyIntervals<I> {
     iter: I,
@@ -32,6 +184,8 @@ where
     I: Iterator,
     I::Item: Emptiable,
 {
+    /// Creates a new [`RemoveEmptyIntervals`]
+    #[must_use]
     pub fn new(iter: I) -> Self {
         RemoveEmptyIntervals { iter }
     }
