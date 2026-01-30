8ecb063a4dc26614cf8c9991792a6040f3184c38
maeldroem <maeldroem@proton.me>
2026-01-30 22:15:49 +0100

    Added naive duration and add/sub of naive duration on naive date + tests
    - Added NaiveDuration
    - Added methods to add and subtract naive durations to a single naive date
    - Added tests on those methods

--- src/time_constants.rs
+++
@@ -1,9 +0,0 @@
-use chrono::NaiveTime;
-
-/// Represents midnight as a [`NaiveTime`]
-pub const NAIVE_TIME_MIDNIGHT: NaiveTime = NaiveTime::from_hms_opt(0, 0, 0)
-    .expect("Provided valid hour/minute/second (hms) combination");
-
-/// Represents noon as a [`NaiveTime`]
-pub const NAIVE_TIME_NOON: NaiveTime = NaiveTime::from_hms_opt(12, 0, 0)
-    .expect("Provided valid hour/minute/second (hms) combination");
