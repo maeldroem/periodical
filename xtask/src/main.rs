932ddcdec1db2305361451bd3c5ce34738f28db1
maeldroem <maeldroem@proton.me>
2026-05-08 21:34:15 +0200

    Added formatting check xtask

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,28 +1,104 @@
-use std::env;
 use std::error::Error;
 use std::ffi::OsStr;
+use std::fmt::Display;
+use std::io;
 use std::path::Path;
 use std::process::{Command, ExitCode};
 use std::sync::LazyLock;
 
 use clap::{Parser, Subcommand};
 
-static COVERAGE_FOLDER: LazyLock<&Path> = LazyLock::new(|| Path::new("./target/coverage/profraw"));
-static PROFILING_FILE_NAME: LazyLock<&Path> = LazyLock::new(|| Path::new("cargo_coverage_%p_%m.profraw"));
-static COVERAGE_TARGET_FOLDER: LazyLock<&Path> = LazyLock::new(|| Path::new("./target/coverage/html"));
+const RUSTUP_BIN_LOCATION: &str = "rustup";
+const GRCOV_BIN_LOCATION: &str = "grcov";
+const OPEN_BIN_LOCATION: &str = "xdg-open";
 
-static CARGO_LOCATION: LazyLock<String> = LazyLock::new(|| env::var("CARGO").unwrap_or("cargo".to_string()));
-static GRCOV_LOCATION: LazyLock<&str> = LazyLock::new(|| "grcov");
-static XDG_OPEN_LOCATION: LazyLock<&str> = LazyLock::new(|| "xdg-open");
+static PROFILING_DATA_FOLDER: LazyLock<&Path> = LazyLock::new(|| Path::new("./target/profiling"));
+static CODE_COVERAGE_REPORT_TARGET: LazyLock<&Path> = LazyLock::new(|| Path::new("./target/coverage"));
 
-const TEST_FILE_GLOB: &str = "**/*_tests.rs";
+const PROFILING_DATA_NAME_TEMPLATE: &str = "cargo_coverage_%p_%m.profraw";
+
+const TEST_FILE_GLOB: &str = "./src/**/*_tests.rs";
 
 /// Line exclusion pattern for `grcov`
 ///
 /// Ignores all attributes, comments, punctuation-full lines, else-clause line,
 /// lines with `unreachable!` invocations, and empty lines.
 const GRCOV_LINE_EXCL_REGEX: &str =
-    r#"^\s*(#\[.+\]|\/{2}.*|[\(\[\{]+|,?[\)\]\}]+[;,]?|\} else \{|.+?unreachable!.+)?\n?$"#;
+    r"^\s*(#\[.+\]|\/{2}.*|[\(\[\{]+|,?[\)\]\}]+[;,]?|\} else \{|.+?unreachable!.+)?\n?$";
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+enum RustToolchain {
+    Stable,
+    Nightly,
+}
+
+impl Display for RustToolchain {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Stable => write!(f, "stable"),
+            Self::Nightly => write!(f, "nightly"),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Hash)]
+struct GetRustBinError;
+
+impl Display for GetRustBinError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "An error occurred when trying to retrieve a Rust binary")
+    }
+}
+
+fn get_rust_bin(bin: &str, toolchain: RustToolchain) -> Result<String, GetRustBinError> {
+    let rustup_which_output = Command::new(RUSTUP_BIN_LOCATION)
+        .arg("which")
+        .arg("--toolchain")
+        .arg(toolchain.to_string())
+        .arg(bin)
+        .output()
+        .or(Err(GetRustBinError))?;
+
+    Ok(String::from_utf8(rustup_which_output.stdout)
+        .or(Err(GetRustBinError))?
+        .trim()
+        .to_string())
+}
+
+#[derive(Debug)]
+enum XtaskError {
+    IoError(io::Error),
+    TaskError(String),
+}
+
+impl Display for XtaskError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::IoError(err) => err.fmt(f),
+            Self::TaskError(err) => err.fmt(f),
+        }
+    }
+}
+
+impl Error for XtaskError {}
+
+impl From<io::Error> for XtaskError {
+    fn from(value: io::Error) -> Self {
+        Self::IoError(value)
+    }
+}
+
+impl From<GetRustBinError> for XtaskError {
+    fn from(value: GetRustBinError) -> Self {
+        Self::TaskError(value.to_string())
+    }
+}
+
+impl From<&str> for XtaskError {
+    fn from(value: &str) -> Self {
+        Self::TaskError(value.to_string())
+    }
+}
 
 #[derive(Parser)]
 struct Cli {
@@ -38,6 +114,8 @@ enum XTask {
         #[arg(long)]
         open: bool,
     },
+    /// Checks source code formatting
+    FmtCheck,
 }
 
 fn main() -> ExitCode {
@@ -51,25 +129,32 @@ fn main() -> ExitCode {
 
 fn try_main() -> Result<(), Box<dyn Error>> {
     let cli = Cli::parse();
-    // let cli = Cli::new("periodical — xtask").arg(Arg::new("open").);
 
     match cli.xtask {
         XTask::Coverage {
             open,
-        } => xtask_coverage(open),
+        } => xtask_coverage(open).map_err(Box::<dyn Error>::from),
+        XTask::FmtCheck => xtask_fmtcheck().map_err(Box::<dyn Error>::from),
     }
 }
 
-fn xtask_coverage(open: bool) -> Result<(), Box<dyn Error>> {
-    let mut llvm_profile_file = COVERAGE_FOLDER.to_path_buf();
-    llvm_profile_file.push(AsRef::<OsStr>::as_ref(&*PROFILING_FILE_NAME));
+fn xtask_coverage(open: bool) -> Result<(), XtaskError> {
+    let mut llvm_profile_file = PROFILING_DATA_FOLDER.to_path_buf();
+    llvm_profile_file.push(PROFILING_DATA_NAME_TEMPLATE);
+
+    eprintln!("Retrieving stable cargo…");
+
+    let stable_cargo = get_rust_bin("cargo", RustToolchain::Stable)?;
+
+    eprintln!("Done.");
 
     eprintln!("Profiling code…");
 
-    let cargo_test_succeed = Command::new(AsRef::<OsStr>::as_ref(&*CARGO_LOCATION))
+    let cargo_test_succeed = Command::new(stable_cargo)
         .env("CARGO_INCREMENTAL", "0")
         .env("RUSTFLAGS", "-C instrument-coverage")
         .env("LLVM_PROFILE_FILE", llvm_profile_file)
+        .arg("+stable")
         .arg("test")
         .arg("--tests") // Only tests, no doc tests
         .arg("--all-features")
@@ -84,7 +169,7 @@ fn xtask_coverage(open: bool) -> Result<(), Box<dyn Error>> {
     eprintln!("Code profiled successfully!");
     eprintln!("Generating code coverage report…");
 
-    let grcov_succeed = Command::new(AsRef::<OsStr>::as_ref(&*GRCOV_LOCATION))
+    let grcov_succeed = Command::new(GRCOV_BIN_LOCATION)
         .arg(".")
         .arg("-b")
         .arg("./target/debug/deps/")
@@ -100,7 +185,7 @@ fn xtask_coverage(open: bool) -> Result<(), Box<dyn Error>> {
         .arg("--ignore")
         .arg(TEST_FILE_GLOB)
         .arg("-o")
-        .arg(AsRef::<OsStr>::as_ref(&*COVERAGE_TARGET_FOLDER))
+        .arg(AsRef::<OsStr>::as_ref(*CODE_COVERAGE_REPORT_TARGET))
         .status()?
         .success();
 
@@ -113,20 +198,63 @@ fn xtask_coverage(open: bool) -> Result<(), Box<dyn Error>> {
     if open {
         eprintln!("Opening code coverage report…");
 
-        let mut report_index = COVERAGE_TARGET_FOLDER.to_path_buf();
+        let mut report_index = PROFILING_DATA_FOLDER.to_path_buf();
         report_index.push("html/index.html");
 
-        let xdg_open_succeed = Command::new(AsRef::<OsStr>::as_ref(&*XDG_OPEN_LOCATION))
-            .arg(report_index)
-            .status()?
-            .success();
+        let open_succeed = Command::new(OPEN_BIN_LOCATION).arg(report_index).status()?.success();
 
-        if !xdg_open_succeed {
+        if !open_succeed {
             return Err("Failed to open code coverage report".into());
         }
 
         eprintln!("Opened!");
     }
+
+    Ok(())
+}
+
+fn xtask_fmtcheck() -> Result<(), XtaskError> {
+    eprintln!("Retrieving stable cargo");
+
+    let stable_cargo = get_rust_bin("cargo", RustToolchain::Stable)?;
+
+    eprintln!("Done.");
+    eprintln!("Running clippy…");
+
+    let clippy_succeed = Command::new(&stable_cargo)
+        .arg("clippy")
+        .arg("-q")
+        .arg("--all-targets")
+        .status()?
+        .success();
+
+    if !clippy_succeed {
+        return Err("Some clippy lints were not respected!".into());
+    }
+
+    eprintln!("No broken clippy lints detected.");
+    eprintln!("Retrieving nightly rustfmt");
+
+    let nightly_rustfmt = get_rust_bin("rustfmt", RustToolchain::Nightly)?;
+
+    eprintln!("Done.");
+    eprintln!("Checking source code formatting…");
+
+    let cargo_fmt_succeed = Command::new(&stable_cargo)
+        .env("RUSTFMT", &nightly_rustfmt)
+        .arg("fmt")
+        .arg("-q")
+        .arg("--check")
+        .arg("--")
+        .arg("--unstable-features")
+        .status()?
+        .success();
+
+    if !cargo_fmt_succeed {
+        return Err("Some source code files were not formatted correctly!".into());
+    }
+
+    eprintln!("Source code is well-formatted.");
 
     Ok(())
 }
